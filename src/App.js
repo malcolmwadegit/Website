7d9d4f1b2a7308093603c08093db29dfec458196
Malcolm <malcolmwade3@outlook.com>
2024-05-05 10:49:27 -0700

    another commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import Home from './pages/Home/Home';
 import ContactPage from './pages/Contact/Contact';
 import './styles/Global.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
+
 
 function App() {
   return (
-    <>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     <div className="app-container">
-    <Router basename="/Website">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact-page" element={<ContactPage />} />
-      </Routes>
-    </Router>
-
-    
-    
+      <Router basename="/Website">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact-page" element={<ContactPage />} />
+        </Routes>
+      </Router>
     </div>
-    </>
   );
 }
 
