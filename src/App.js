6a1151005ac4a13a3912acea9a36a8522a9e786f
Malcolm <malcolmwade3@outlook.com>
2024-05-05 12:35:50 -0700

    trying again

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import Home from './pages/Home/Home';
 import ContactPage from './pages/Contact/Contact';
 import './styles/Global.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app-container">
-      <Router basename="/website">
+    <Router basename="/website">
+      <div className="app-container">
         <Routes>
-          <Route path="/website" component={Home} />
-          <Route path="/website/contact-page" component={ContactPage} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contact-page" element={<ContactPage />} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
